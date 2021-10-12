3cd132148043b2a17027c9343c78b2506b9a6346
Philipp Schmitt <philipp@schmitt.co>
2021-10-12 08:16:44 +0200

    Add docker-run.sh

---
+++ docker-run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)" || exit 9
+
+source .envrc
+
+docker run -it --rm -p 8000:8000 \
+  -e "CONFLUENCE_URL=${CONFLUENCE_URL}" \
+  -e "CONFLUENCE_USERNAME=${CONFLUENCE_USERNAME}" \
+  -e "CONFLUENCE_PASSWORD=${CONFLUENCE_PASSWORD}" \
+  -e "EXCHANGE_USERNAME=${EXCHANGE_USERNAME}" \
+  -e "EXCHANGE_PASSWORD=${EXCHANGE_PASSWORD}" \
+  pschmitt/jcalapi
