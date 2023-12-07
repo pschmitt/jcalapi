7c7627c09414da95cc5f786837b6080d772be4a6
Philipp Schmitt <philipp.schmitt@german-edge-cloud.io>
2023-12-07 07:35:37 +0100

    80 chars

--- jcalapi/backend/exchange.py
+++ jcalapi/backend/exchange.py
@@ -209,13 +209,20 @@ def sync_get_exchange_events(
             ev_status = "cancelled" if ev.is_cancelled else "confirmed"
 
             ev_attendees = []
-            for attendee_list in [ev.required_attendees, ev.optional_attendees]:
+            for attendee_list in [
+                ev.required_attendees,
+                ev.optional_attendees,
+            ]:
                 if isinstance(attendee_list, list):
-                    ev_attendees.extend([
-                        {"name": attendee.mailbox.name, "email": attendee.mailbox.email_address}
-                        for attendee in attendee_list
-                    ])
-
+                    ev_attendees.extend(
+                        [
+                            {
+                                "name": attendee.mailbox.name,
+                                "email": attendee.mailbox.email_address,
+                            }
+                            for attendee in attendee_list
+                        ]
+                    )
 
             ev_data = {
                 "uid": ev.uid,
