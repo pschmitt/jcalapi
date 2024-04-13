755458e74d1f236985a8ea687484877eeb60099e
Philipp Schmitt <philipp.schmitt@wiit.cloud>
2024-04-13 16:09:31 +0200

    Add devenv

---
+++ devenv.nix
@@ -0,0 +1,20 @@
+{ ... }:
+
+{
+  languages.nix.enable = true;
+  languages.python.enable = true;
+
+  languages.python.poetry = {
+    enable = true;
+    activate = {
+      enable = true;
+    };
+  };
+
+  pre-commit.hooks = {
+    black.enable = true;
+    isort.enable = true;
+  };
+
+  devcontainer.enable = true;
+}
